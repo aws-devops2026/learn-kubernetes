e6b3cb37e9be639cd7d560388a3153391a212775
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-21 16:51:12 +0530

    terraform

---
+++ roboshop-v6-eks/modules/eks/nodeGroup.tf
@@ -0,0 +1,28 @@
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "example"
+  node_role_arn   = aws_iam_role.example.arn
+ subnet_ids = [
+      "subnet-0d1a07bc7ceaf4694",
+      "subnet-05a9dc77897b66c38",
+      "subnet-08c53c78664626d0f"
+    ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 5
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
