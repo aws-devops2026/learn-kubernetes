c6553e238c7fb7b47da57dd8efa66d2b94db871d
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-21 16:31:37 +0530

    terraform

---
+++ roboshop-v6-eks/modules/data.tf
@@ -0,0 +1,28 @@
+# aws ami datasource
+data "aws_ami" "latest" {
+    most_recent = true
+
+
+    filter {
+        name   = "name"
+        values =["${var.ami_name}"] 
+        
+        # Name of the AMI, you can find it in the AWS console or by using AWS CLI
+    }   
+
+ # owners = ["self"]
+
+ #  owners = ["276719382023"]  # ownerID
+
+}
+
+# Datasource for security group
+data "aws_security_group" "selected" {
+  name = var.sg_name
+}
+
+# Datasource for route53 zone, 
+data "aws_route53_zone" "selected" {
+  name         = var.domain_name
+  private_zone = false
+}
---
+++ roboshop-v6-eks/vars.tf
@@ -0,0 +1,6 @@
+variable "env_name" {}
+variable "ami_name" {}
+variable "sg_name" {}
+variable "components" {}
+variable "domain_name" {}
+
