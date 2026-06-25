228f78d69c8147c121e80c99c87a20a2fd62932e
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-25 22:36:17 +0530

    k8s -provisions eks cluster and nogegroup

---
+++ roboshop-7eks/modules/main.tf
@@ -0,0 +1,21 @@
+resource "aws_eks_cluster" "example1" {
+  name = "example1"
+
+  role_arn = aws_iam_role.cluster.arm
+  version = "1.35"
+
+vpc_config {
+  subnet_ids = [
+    "subnet-05ea2f5637b5f7f8c"
+    "subnet-099c5c7842b75d921"
+    "subnet-072e4160e181ae39d"
+    ]
+}
+
+depends_on = [ 
+
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+ ]
+}
