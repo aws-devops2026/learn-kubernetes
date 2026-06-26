76d5ba3ac41734354ba97175bb045a88f8433254
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-26 16:10:27 +0530

    k8s -provisions eks cluster and nodegroup

--- roboshop-7eks/modules/main.tf
+++ roboshop-7eks/modules/main.tf
@@ -1,9 +1,7 @@
 resource "aws_eks_cluster" "example1" {
   name = "example1"
-
   role_arn = aws_iam_role.cluster.arm
   version = "1.35"
-
 vpc_config {
   subnet_ids = [
     "subnet-05ea2f5637b5f7f8c",
@@ -11,14 +9,12 @@ vpc_config {
     "subnet-072e4160e181ae39d"
     ]
 }
-
 depends_on = [ 
 
-
-   # aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+   #  aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
    #  aws_iam_role_policy_attachment.cluster_AmazonEKSComputePolicy,
    #  aws_iam_role_policy_attachment.cluster_AmazonEKSBlockStoragePolicy,
-   # aws_iam_role_policy_attachment.cluster_AmazonEKSLoadBalancingPolicy,
-   # aws_iam_role_policy_attachment.cluster_AmazonEKSNetworkingPolicy,
+   #  aws_iam_role_policy_attachment.cluster_AmazonEKSLoadBalancingPolicy,
+   #  aws_iam_role_policy_attachment.cluster_AmazonEKSNetworkingPolicy,
  ]
 }
